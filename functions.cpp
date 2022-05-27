3853a1c0bd141548c6a42d2f092606adf278f646
Lost Control <73237580+LControl-code@users.noreply.github.com>
2022-05-27 19:26:01 +0200

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,52 @@
+// #include <iostream>
+// using namespace std;
+// int addition(int a, int b) {
+// 	cin>>a>>b;
+// 	return a + b;
+// }
+// int main() { 
+//     int a, b;
+//     addition(a,b);
+//     return 0;   
+
+// }
+#include <iostream>
+#include <string>
+// #include <vector>
+// #include <algorithm> what even is this????
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+// int main()
+// {
+//     string s;
+//     char min = 'z';
+//     getline(cin, s);
+
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         if (s[i] <= min && int(s[i])>=65){
+//             min = s[i];
+//         }
+        
+//     }
+    
+//     for(int i = 0; i < s.length(); i++)
+//     {
+//         if (s[i] != min)
+//             cout << s[i];
+//     }
+// }
+// function that return the number of odd numbers
+int odd_count(long long n){
+// return n/2; // is also the number of even numbers
+return n/2; // is the number of odd numbers rounded down
+}
+int main(){
+    odd_count(101285326932);
+}
---
+++ student_avarage.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<float>> sort_simple(vector<vector<float>> grade_not_sorted, int r){
+        for (int i = 1; i < r; i++){
+                // float current = grade_not_sorted[i][5];
+                // insertion sort
+                vector<float> current_arr(r);
+                current_arr= grade_not_sorted[i];
+                int j = i - 1;
+                while (j >= 0 && grade_not_sorted[j][5] > current_arr[5]){
+                        grade_not_sorted[j+1] = grade_not_sorted[j];
+
+                        j--;
+                }
+                grade_not_sorted[j + 1] = current_arr;
+        }
+        return grade_not_sorted;
+}
+
+void list_ranking(vector<vector<float>> grade_to_rank, int r) {
+        cout << "RANKS  STUDENT NO.  AVERAGE" <<  endl;
+        int rank = 1;
+        for (int i = r - 1; i >= 0; i--) {
+                cout << rank << "\t" << grade_to_rank[i][0] << "\t     " << grade_to_rank[i][5] << endl;
+                rank++;
+        }
+}
+int main() {
+        int student_count;
+        cout << "How many students do you want to input? ";
+        cin >> student_count;
+
+        vector<vector<float>> grade(student_count, vector<float>(5));
+        for (int i=0; i<student_count; i++){
+        cout << "\nInput Average Grade of Student #" << i+1 << endl;
+        grade[i][0] = i + 1;
+
+        cout << "First Grading Period: ";
+        cin >> grade[i][1];
+
+        cout << "Second Grading Period: ";
+        cin >>grade[i][2] ;
+
+        cout << "Third Grading Period: ";
+        cin >> grade[i][3];
+
+        cout << "Fourth Grading Period: ";
+        cin >> grade[i][4];
+
+        grade[i].push_back((grade[i][1] + grade[i][2] + grade[i][3] + grade[i][4]) / 4);
+                
+        cout << "=============================" << endl;
+        cout << "Average of Student #" << i+1 << ": " << grade[i][5] << endl;
+        system("clear");
+        }
+
+        grade = sort_simple(grade, student_count);
+        list_ranking(grade, student_count);
+        return 0;
+}
