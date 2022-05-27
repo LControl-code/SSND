42577983e05ba8ea6e6cc9503fb21f4b7fde4eb9
Lost Control <73237580+LControl-code@users.noreply.github.com>
2022-05-27 19:15:24 +0200

    Create n_23-NAOPAK_CISLO-23.4.2022.cpp

---
+++ n_23-NAOPAK_CISLO-23.4.2022.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num = 0;
+	cin >> num;
+	while (num != 0){
+		if (num % 10 != 0)
+			cout << num % 10;
+		num /= 10;
+	}
+	return 0;
+}
