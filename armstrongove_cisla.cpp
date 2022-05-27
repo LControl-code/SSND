eb161c6e58339d444508c7ad89e5e9caffe5a5c6
Lost Control <73237580+LControl-code@users.noreply.github.com>
2022-05-27 18:59:53 +0200

    Add files via upload

---
+++ armstrongove_cisla.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+	// abc = a^3 + b^3 + c^3
+	// int num;
+	// std::cin >> num;
+	for (int incrementation_num = 100; incrementation_num <= 999; incrementation_num++) {
+		// std::vector<int> reversed_array(3);
+		int temp_num = incrementation_num, ending_num = 0;
+		for (int arr_num_inc = 2; arr_num_inc >= 0; arr_num_inc--) {
+			// reversed_array[arr_num_inc] =  % 10;
+            int plus_num = temp_num % 10; 
+			ending_num += plus_num*plus_num*plus_num;
+			temp_num /= 10; // temp_num = temp_num / 10
+
+		}
+
+		if (ending_num == incrementation_num) {
+			std::cout << incrementation_num << " ";
+		}
+	}
+
+        std::cout << std::endl;
+	return 0;
+}
