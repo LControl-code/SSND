390a902bddfc2825f78f5fe4ea432b90882b57d0
Lost Control <lcontrol2326@gmail.com>
2022-05-30 19:07:49 +0200

    2d

--- cpp_course/2d_arrays.cpp
+++ cpp_course/2d_arrays.cpp
@@ -5,11 +5,20 @@ void test();
 
 int main() {
 
-        for(int i =1; i < 5; i++) {
-                
-        }
-        
-        cout << "How are you";
-        return 0;
-}
+// * we can inicialize 2d array like this
+  int numberGrid[3][2] = {
+                                        {1, 2},
+                                        {3, 4},
+                                        {5, 6}
+                                }; 
+  
+  cout << numberGrid[0][1];
+  cout << numberGrid[1][0];
 
+  for(int i =0;i<3;i++){
+          for(int j=0;j<2;j++){
+                  cout << numberGrid[i][j];
+          }
+  }
+  return 0;
+}
