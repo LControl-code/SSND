dd663056eea204df30187ea3a262b31986a35555
Lost Control <lcontrol2326@gmail.com>
2022-05-30 21:27:06 +0200

    course

---
+++ cpp_course/comments.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // inline comment
+  // ! alert
+  // * emphasis
+  // ? Note or query
+  // TODO just todo
+  /**
+   * * Important information is higlighted
+   * ! Deprecated method, do not use
+   * ? Should this method be exposed to the public API?
+   * TODO: refactor the code so that it conforms with the API
+   * @param myParam it's an parameter for this method
+   *
+   */
+
+  //// cout << "Comments are not fun";
+
+  cout << "Comments are fun";
+
+  return 0;
+}
