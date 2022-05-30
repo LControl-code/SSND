0dec6ae18cfcbb4e224295f9a8cebbd7df2fa35d
Lost Control <lcontrol2326@gmail.com>
2022-05-30 17:27:35 +0200

    function explanation

--- cpp_course/functions.cpp
+++ cpp_course/functions.cpp
@@ -1,7 +1,27 @@
+// inicialization of a function
 #include <iostream>
 using namespace std;
 
+// referencing the function (prototype)
+string sayHi(string _name);
+
 int main(void) {
 
+        string name;
+        cin >> name;
+        string mood = sayHi(name);
+
+        cout << "\n you are " << mood;
         return EXIT_SUCCESS;
+}
+
+string sayHi(string _name) {
+        cout << "Hello " << _name << endl;
+
+        cout << "How are you?";
+
+        string mood;
+        cin >> mood;
+
+        return mood;
 }
