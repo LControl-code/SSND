c9c8f612f3924ded67501b51a4b33f01ba06b6be
Lost Control <lcontrol2326@gmail.com>
2022-05-30 19:11:12 +0200

    s

--- cpp_course/2d_arrays.cpp
+++ cpp_course/2d_arrays.cpp
@@ -17,7 +17,7 @@ int main() {
 
   for(int i =0;i<3;i++){
           for(int j=0;j<2;j++){
-                  cout << numberGrid[i][j];
+                  cout << numberGrid[i][j] << ' ';
           }
   }
   return 0;
