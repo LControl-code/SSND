ceb3b700ca6ccf0cfcbe6f554612d368cbc64f6d
Lost Control <lcontrol2326@gmail.com>
2022-05-30 18:14:49 +0200

    recreation of pow

---
+++ cpp_course/exponent.cpp
@@ -0,0 +1,19 @@
+// recreation of pow function
+#include <iostream>
+using namespace std;
+
+long long power(long long baseNum, int powNum) {
+        int result = 1;
+
+        for(int i = 0; i < powNum; i++){
+                result *= baseNum;
+        }
+
+        return result;
+}
+
+int main(){
+
+        cout << power(2, 16);
+        return 0;
+}
