5e25c6c52cc9d4dfcbf79ee71ba08c8ca4266f18
Lost Control <lcontrol2326@gmail.com>
2022-05-30 18:15:03 +0200

    new

---
+++ cpp_course/better_calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int num1, num2;
+        char op;
+
+        cout << "Enter your first number: ";
+        cin >> num1;
+
+        cout << "Enter the operator: ";
+        cin >> op;
+
+        cout << "Enter your second number: ";
+        cin >> num2;
+
+        int result;
+        switch(op) {
+                case '+' :
+                      result = num1 + num2;
+                      break;
+                case '-' :
+                        result = num1 - num2;
+                        break;
+                case '*' :
+                        result = num1 * num2;
+                        break;
+                case '/' :
+                        result = num1 / num2; 
+                        break;
+                default :
+                        cout << "Invalid operator";
+                        return 0;
+        }
+
+        cout << "Result: " << result;
+        return 0;
+}
