a48812b64720273fcadd856eb1d73d282552b94f
Lost Control <lcontrol2326@gmail.com>
2022-06-02 15:03:05 +0200

    update

--- passign_parameters.cpp
+++ passign_parameters.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  for(int i = 0; i < argc; ++i) {
-    cout << "argv[" << i << "] = " << argv[i] << endl;
+  for(int i = 0; i < argc; ++i) { // argc is the number of arguments passed to the program
+    cout << "argv[" << i << "] = " << argv[i] << endl; // argv is an array of strings that are the arguments passed to the program (argv[0] is the name of the program)
   }
 
  // return is not needed here, but it is a good habit to have
