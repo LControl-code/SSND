d2dd7d36ed184812176a96109797dec05d653435
Lost Control <lcontrol2326@gmail.com>
2022-06-02 15:29:28 +0200

    final

--- passign_parameters.cpp
+++ passign_parameters.cpp
@@ -1,12 +1,31 @@
+// program to pass parameters to a function and print them out
+// can give help to users if specified "--help"
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  for(int i = 0; i < argc; ++i) { // argc is the number of arguments passed to the program
-    cout << "argv[" << i << "] = " << argv[i] << endl; // argv is an array of strings that are the arguments passed to the program (argv[0] is the name of the program)
+  string arg1[argc - 1];
+
+  if (argc > 1) {
+    arg1[0] = argv[1]; // arg1 is the first argument passed to the program
+
+    if (arg1[0] == "--help") {
+      cout << "This program takes a string as an argument and prints it."
+           << endl;
+      cout << "Usage: " << argv[0] << " <string>" << endl;
+      return 0;
+    } else {
+      for (int i = 0; i < argc; i++) {
+        // argc is the number of arguments passed to the program
+        cout << "argv[" << i << "] = " << argv[i] << endl;
+        // argv is an array of strings that are the arguments passed
+        // to the program (argv[0] is the name of the program)
+      }
+    }
   }
 
- // return is not needed here, but it is a good habit to have
+  // return is not needed here, but it is a good habit to have
   return 0;
 }
