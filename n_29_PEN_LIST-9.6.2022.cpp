b33f78fcbd24271ea81ad81769e19a1af7ebb5a0
Lost Control <lcontrol2326@gmail.com>
2022-06-16 10:55:03 +0200

    formatting

--- n_29_PEN_LIST-9.6.2022.cpp
+++ n_29_PEN_LIST-9.6.2022.cpp
@@ -15,57 +15,58 @@ int main() {
   std::string first_name[MAX] = {};
   int pocet = 0;
   int name_list[MAX] = {};
-  bool new_name = true;
   
-  // ucitelova metoda, lahsia
-  std::string text;
-  while (file_R >> name[pocet]) {
-    std::getline(file_R, text); 
+  /*
+  * MOJA METODA
+  */
+  while (std::getline(file_R, name[pocet])) {
+    for (int i = 0; i < name[pocet].length(); i++) {
+
+      if (name[pocet][i] == ' ') {
+        break;
+      } else {
+        first_name[pocet] += name[pocet][i];
+      }
+    }
     int i = 0;
-    while (name[i] != name[pocet]) // najde poziciu v poli pre danu meno
+    while (first_name[i] != first_name[pocet])
       i++;
 
-    if (i == pocet) { // ak nenajde, prida nove meno do pola
+    if (i == pocet) {
       pocet++;
+    } else {
+      first_name[pocet] = {};
     }
 
     name_list[i]++;
   }
-
   file_R.close();
 
-  for (int i = 0; i < pocet; i++) { // vypis vsetkych mien
-    std::cout << name[i] << ": " << name_list[i] << std::endl;
+  for (int i = 0; i < pocet; i++) {
+    std::cout << first_name[i] << ": " << name_list[i] << std::endl;
   }
-  // 
-  // MOJA METODA
-  //
-  // while (std::getline(file_R, name[pocet])) {
-  //   for (int i = 0; i < name[pocet].length(); i++) {
 
-  //     if (name[pocet][i] == ' ') {
-  //       break;
-  //       new_name = true;
-  //     } else {
-  //       first_name[pocet] += name[pocet][i];
-  //     }
-  //   }
+  /*
+   * UCITELOVA METODA
+  */
+  // std::string text;
+  // while (file_R >> name[pocet]) {
+  //   std::getline(file_R, text); 
   //   int i = 0;
-  //   while (first_name[i] != first_name[pocet])
+  //   while (name[i] != name[pocet]) // najde poziciu v poli pre danu meno
   //     i++;
 
-  //   if (i == pocet) {
+  //   if (i == pocet) { // ak nenajde, prida nove meno do pola
   //     pocet++;
-  //   } else {
-  //     first_name[pocet] = {};
   //   }
 
   //   name_list[i]++;
   // }
+
   // file_R.close();
 
-  // for (int i = 0; i < pocet; i++) {
-  //   std::cout << first_name[i] << ": " << name_list[i] << std::endl;
+  // for (int i = 0; i < pocet; i++) { // vypis vsetkych mien
+  //   std::cout << name[i] << ": " << name_list[i] << std::endl;
   // }
   
 
