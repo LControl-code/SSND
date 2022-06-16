bab3b6b9a10f269dabb79764bf25e5fa7c1a7970
Lost Control <lcontrol2326@gmail.com>
2022-06-16 11:50:57 +0200

    update

--- n_XX-STRANA_TROJUHOLNIKA-16.6.2022.cpp
+++ n_XX-STRANA_TROJUHOLNIKA-16.6.2022.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// void printTriangle(int n) {
-//   for (int i = 0; i < n; i++) {
-//     for (int j = 0; j < (n - 1) - i; j++) { // output whitespaces (n - 1) = 6
-//       cout << " ";
-//     }
+void printTriangle_up(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < (n - 1) - i; j++) { // output whitespaces (n - 1) = 6, keeping the last place for atherisk (*)
+      cout << " ";
+    }
 
-//     for (int j = 0; j <= i; j++) { // output the triangle
-//       if (j == 0 || j == i || i == n - 1) {
-//         cout << "* ";
-//       } else {
-//         cout << "  ";
-//       }
+    for (int j = 0; j <= i; j++) { // output the triangle
+      if (j == 0 || j == i || i == n - 1) {
+        cout << "* ";
+      } else {
+        cout << "  ";
+      }
 
-//     }
-//     cout << endl;
-//   }
-// }
+    }
+    cout << endl;
+  }
+}
 
-void printTriangle(int n) {
+void printTriangle_down(int n) {
   for (int i = n; i >= 0; i--) {
-    for (int j = 0; j < n - i; j++) { // output whitespaces (n - 1) = 6
+    for (int j = 0; j < n - i; j++) { // output whitespaces (n - 1) = 6, keeping the last place for atherisk (*) on every row
       cout << " ";
     }
 
@@ -39,9 +39,56 @@ void printTriangle(int n) {
 
 int main() {
 
+  cout << "Enter 1 to print the triangle up, 2 to print the triangle down: ";
+  int choice;
+  cin >> choice;
+
   cout << "Enter the number of rows: ";
   int n;
   cin >> n;
-  printTriangle(n);
+  cout << endl;
+
+  bool run = true;
+  switch (choice) {
+  case 1:
+    while (run) {
+      printTriangle_up(n);
+
+      cout << endl << "Continue? (y/n): ";
+      char c;
+      cin >> c;
+
+      if (c == 'n' || c == 'N') {
+        run = false;
+        continue;
+      }
+
+      cout << endl;
+      cout << "Enter the number of rows: ";
+      cin >> n;
+      cout << endl;
+    }
+    break;
+
+  case 2:
+    while (run) {
+      printTriangle_down(n);
+
+      cout << endl << "Continue? (y/n): ";
+      char c;
+      cin >> c;
+
+      if (c == 'n' || c == 'N') {
+        run = false;
+        continue;
+      }
+
+      cout << endl;
+      cout << "Enter the number of rows: ";
+      cin >> n;
+      cout << endl;
+    }
+    break;
+  }
   return 0;
 }
