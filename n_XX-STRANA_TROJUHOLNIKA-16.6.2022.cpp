8cadb12e2422df628acbd7351b2e387871b7092b
Lost Control <lcontrol2326@gmail.com>
2022-06-16 11:34:16 +0200

    triangle

--- n_XX-STRANA_TROJUHOLNIKA-16.6.2022.cpp
+++ n_XX-STRANA_TROJUHOLNIKA-16.6.2022.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 using namespace std;
 
+// void printTriangle(int n) {
+//   for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < (n - 1) - i; j++) { // output whitespaces (n - 1) = 6
+//       cout << " ";
+//     }
+
+//     for (int j = 0; j <= i; j++) { // output the triangle
+//       if (j == 0 || j == i || i == n - 1) {
+//         cout << "* ";
+//       } else {
+//         cout << "  ";
+//       }
+
+//     }
+//     cout << endl;
+//   }
+// }
+
 void printTriangle(int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < (n - 1) - i; j++) { // output whitespaces
+  for (int i = n; i >= 0; i--) {
+    for (int j = 0; j < n - i; j++) { // output whitespaces (n - 1) = 6
       cout << " ";
     }
 
     for (int j = 0; j <= i; j++) { // output the triangle
-      if (j == 0 || j == i || i == n - 1) {
+      if (j == 0 || j == i || i == n) {
         cout << "* ";
       } else {
         cout << "  ";
