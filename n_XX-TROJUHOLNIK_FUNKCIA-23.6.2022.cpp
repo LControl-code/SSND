8a40a9bb86dc537da837b43facb6c8c075425fc0
Lost Control <lcontrol2326@gmail.com>
2022-06-23 09:17:33 +0200

    DONE

--- n_XX-TROJUHOLNIK_FUNKCIA-23.6.2022.cpp
+++ n_XX-TROJUHOLNIK_FUNKCIA-23.6.2022.cpp
@@ -7,6 +7,7 @@ void KresliTrojuholnik(int n);
 
 int main() {
   fstream file_R("TXT-vstup-2.txt", ios::in);
+  if(!file_R.is_open()) return 1;
   int n;
 
   while (file_R >> n) {
@@ -22,14 +23,14 @@ void KresliTrojuholnik(int n) {
   for (int i = 0; i < n; i++) {
 
     for (int j = 0; j < n - i; j++) { // whitespace
-      cout << "  ";
+      cout << "  "; // 2 spaces
     }
 
     for (int j = 0; j <= i; j++) { // asterisks
       if (j == 0 || i == n - 1 || j == i)
         cout << "* ";
       else
-        cout << "  ";
+        cout << "  "; // 2 spaces
     }
     cout << endl;
   }
