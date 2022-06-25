9e01284c885a13fa40e616aa8ceba708097f8fb5
Lost Control <lcontrol2326@gmail.com>
2022-06-25 09:38:48 +0200

    polymorphosis

--- cpp_course/OOP/n_1-OOP-23.6.2022.cpp
+++ cpp_course/OOP/n_1-OOP-23.6.2022.cpp
@@ -10,9 +10,10 @@ class AbstractEmployee {
 
 class Employee: AbstractEmployee {
 private:
-  string Name;
   string Company;
   int Age;
+protected:
+  string Name;
 public:
   Employee(string name, string company, int age) {
     Name = name;
@@ -61,57 +62,100 @@ public:
     }
   }
 
+  void Work() {
+    cout << Name << " is checking email, task backlog, performing tasks..." << endl;
+  }
 }; // end of class Employee, now we can use Employee in other files
 
 
-class Developer: Employee {
+class Developer:public Employee {
   public:
   string FavProgrammingLanguage; // favorite programming
-  // void AskForPromotion() {
-  //   if(getAge() >= 30) {
-  //     cout << getName() << " got promoted" << endl;
-  //   } else {
-  //     cout << getName() << ", sorry NO promotion for you" << endl;
-  //   }
-  // }
   Developer(string name, string company, int age, string favProgrammingLanguage)
     :Employee(name, company, age){
       FavProgrammingLanguage = favProgrammingLanguage; // the other constructor is calling the base class constructor
     }
 
-
   void FixBug() {
-    cout << getName() << " fixed bug using " << FavProgrammingLanguage << endl;
+    cout << Name << " fixed bug using " << FavProgrammingLanguage << endl;
   }
+
+  void Work() {
+    cout << Name << " is coding " << FavProgrammingLanguage << " programs" << endl;
+  }
+};
+
+
+class Teacher:public Employee {
+  private:
+  string Subject;
+  protected:
+  public:
+
+  Teacher(string name, string company, int age, string subject)
+    :Employee(name, company, age) { // passing the parameters to the base class constructor
+      Subject = subject;
+    }
+
+  void PrepareLesson() {
+    cout << Name << " is preparing lesson on " << Subject << endl;
+  }
+
+  void Work() {
+    cout << Name << " is teaching " << Subject << endl;
+  }
+
 };
 
 int main() {
   Employee employee1("John", "Google", 30);
-  // employee1.Name = "John";
+  // employee1.Name = "John"; // when we use setters, we don't need to use the dot operator
   // employee1.Company = "Google";
   // employee1.Age = 30;
   employee1.setName("Mery");
   employee1.setAge(13); // this will not work because of the if statement in setAge
   employee1.IntroduceYourself();
   employee1.Print();
+  employee1.Work();
   employee1.AskForPromotion();
+
+  cout << "\n\n\n";
 
   Employee employee2  ("Saldina", "YT-CodeBeaty", 25);
   // employee2.Name = "Saldina";
   // employee2.Company = "YT-CodeBeaty";
-  // employee2.Age = 25; 
+  // employee2.Age = 25;
   employee2.IntroduceYourself();
   employee2.Print();
+  employee2.Work();
   employee2.AskForPromotion();
 
+  cout << "\n\n\n";
+
   Employee employee3 = Employee("Jonas", "Facebook", 65);
+  employee3.IntroduceYourself();
   employee3.Print();
+  employee3.Work();
   employee3.AskForPromotion();
 
 
-
+  cout << "\n\n\n";
 
   Developer developer1("Maria", "Google", 30, "C++");
+  developer1.IntroduceYourself();
+  developer1.Print();
   developer1.FixBug();
+  developer1.Work();
+  developer1.AskForPromotion();
+
+  cout << "\n\n\n";
+
+  Teacher teacher1("Mark", "Menza", 55, "Math");
+  teacher1.IntroduceYourself();
+  teacher1.Print();
+  teacher1.PrepareLesson();
+  teacher1.Work();
+  teacher1.AskForPromotion();
+
   return 0;
 } // ended 43:50 out of 1:30:25
