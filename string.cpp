807cb2b1db191cc32dfd2379b70f67c5e812ddfc
Lost Control <lcontrol2326@gmail.com>
2022-06-27 08:21:08 +0200

    codewars

--- string.cpp
+++ string.cpp
@@ -1,28 +1,34 @@
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 
-string uncensor(const string& infected, const string& discovered) {
-    int n = 0;
-    string done;
-    for(long unsigned int i = 0; i < infected.length(); i++) {
-      
-      if (infected[i] == '*') {
-        done += discovered[n];
-        n++;
-      } else {
-        done += infected[i];
-      }
-      
+void uncensor(string &infected, const string discovered) {
+  int n = 0;
+  for (long unsigned int i = 0; i < infected.length(); i++) {
+    if (infected[i] == '*') {
+      infected[i] = discovered[n];
+      n++;
     }
-  
-  
-    return done;
+  }
 }
 
-int main () {
+int main() {
+  
+  string name = "A*dr*a*";
+  uncensor(name, "nes");
+  cout << name << endl;
+  
+  int x = 50;
+  int &Y = x;
+  Y = 100;
+  cout << x << endl;
 
-  string test = uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae");
-  cout << test << endl;
+  int y = 50;
+  int *Z = &y; // Z is a pointer to y (the address of y)
+  *Z = 100; // y is now 100 - * is the dereference operator
+  cout << y << endl;
+
+
   return 0;
 }
+
