704c515d59d1983c5394866c8acf4095ffa73cf6
Lost Control <lcontrol2326@gmail.com>
2022-07-10 13:00:47 +0200

    tests

--- class_vs_struct.cpp
+++ class_vs_struct.cpp
@@ -1,4 +1,6 @@
+#include <cwchar>
 #include <iostream>
+#include <iterator>
 //* there's no difference between a class and a struct
 //! struct is by default public
 //! class is by default private
@@ -6,15 +8,12 @@
 struct Vec2 {
   float x, y;
 
-  void Add(const Vec2& other) {
+  void Add(const Vec2 &other) {
     x += other.x;
     y += other.y;
   }
 
 }; // don't use inheritance for structs, only for data
 
-
-int main()
-{
-
+int main() {
 }
--- random_num.cpp
+++ random_num.cpp
@@ -9,10 +9,13 @@ int main() {
   int min, max;
   cin >> min >> max;
   int i_rand_number;
-  for(int i = 0; i < 50; i++) {
-    i_rand_number = min + rand() % (max - min + 1); // works, je to dobry vzorec, nie ako ten na hodine
+  for (int i = 0; i < 50; i++) {
+    i_rand_number =
+        min + rand() % (max - min +
+                        1); // works, je to dobry vzorec, nie ako ten na hodine
     cout << i_rand_number << ' ';
   }
 
+  std::cout << i_rand_number << '\n';
   return EXIT_SUCCESS;
 }
